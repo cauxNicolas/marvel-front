59fb45313cd43cec104c433a959e53d6f8eb2f04
cauxNicolas <nicaux95@gmail.com>
2020-07-29 17:05:29 +0200

    initialisation OK

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(process.env.REACT_APP_MARVEL);
+      setData(response.data);
+    };
+    setIsLoading(false);
+    fetchData();
+  }, []);
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isLoading === true ? <p>chargement en cours ...</p> : <div>ok</div>}
     </div>
   );
 }

ee60dcd5c192599496b9447bea20706f3723e0b4
cauxNicolas <nicaux95@gmail.com>
2020-08-10 12:20:23 +0200

    help

--- src/components/PersosCard.js
+++ src/components/PersosCard.js
@@ -1,13 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 
 const PersosCard = ({ url, extension, alt, description, id }) => {
   const [favoris, setFavoris] = useState("star");
+  const [favorisStar, setFavorisStar] = useState("far fa-star");
+
+  useEffect(() => {
+    if (Cookies.get("favoris")) {
+      let cookie = Cookies.get("favoris");
+      const numberTab = [];
+      const cookieTab = cookie.split("-");
+      for (let i = 0; i < cookieTab.length; i++) {
+        numberTab.push(Number(cookieTab[i]));
+      }
+      for (let i = 0; i < numberTab.length; i++) {
+        if (numberTab[i] === id) {
+          console.log(numberTab[i]);
+          console.log(id);
+          setFavoris("starRed");
+          setFavorisStar("fas fa-star");
+        } else {
+          setFavoris("star");
+          setFavorisStar("far fa-star");
+        }
+      }
+    }
+  }, []);
 
   // check favoris
   const goToFavoris = (event) => {
     event.preventDefault();
     setFavoris(!favoris);
+    setFavorisStar(!favorisStar);
 
     let currentFavorites = Cookies.get("favoris");
 
@@ -26,6 +50,7 @@ const PersosCard = ({ url, extension, alt, description, id }) => {
         });
       } else {
         let idIndex = favoritesTab.indexOf(stringifiedId);
+
         favoritesTab.splice(idIndex, 1);
         let newFavorites = favoritesTab.join("-");
         Cookies.set("favoris", newFavorites, {
@@ -52,7 +77,7 @@ const PersosCard = ({ url, extension, alt, description, id }) => {
         )}
       </div>
       <div className={favoris ? "star" : "starRed"} onClick={goToFavoris}>
-        <i className={favoris ? "far fa-star" : "fas fa-star"}></i>
+        <i className={favorisStar ? "far fa-star" : "fas fa-star"}></i>
       </div>
     </>
   );
